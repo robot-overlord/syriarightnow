a973f489d265c0f4f5baa3357eaf5a772a825821
Jenn Cooper <cooper.jennl@gmail.com>
2015-10-03 15:46:06 -0700

    Use the correct env variable name

--- tweetstream.py
+++ tweetstream.py
@@ -7,7 +7,7 @@ from tweepy.streaming import StreamListener
 from listener import Listener
 
 ckey = os.environ['CKEY']
-consumer_secret = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
 access_token_key = os.environ['ACCESS_TOKEN_KEY']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
