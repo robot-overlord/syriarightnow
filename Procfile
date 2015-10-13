d4620890f1c28289bc50ab26e5171e5ae6abd66d
Jenn Cooper <cooper.jennl@gmail.com>
2015-10-13 08:20:23 -0700

    Remove unused code

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn syriarightnow:app --log-file=-
-worker: python ./tasks/worker.py
 tweetstream: python ./tasks/enqueue_tweetstream.py
--- tasks/enqueue_tweetstream.py
+++ tasks/enqueue_tweetstream.py
@@ -1,8 +1,3 @@
-# from rq import Queue
-# from worker import conn
 import tweetstream
-#
-# q = Queue(connection=conn)
-# result = q.enqueue(tweetstream.call())
 
 tweetstream.call()
