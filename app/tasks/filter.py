1037f18bbd2f0add6e9e8f6e7a20f26ff3bc57eb
Jenn Cooper <cooper.jennl@gmail.com>
2015-10-17 17:02:09 -0600

    Extract filter logic out into own module

---
+++ app/tasks/filter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+keywords = [
+    u"كيماوي",
+    u"غاز سام",
+    u"كلور",
+    u"اختناق",
+    u"سام",
+    u"غازات سامة",
+    u"الكلور",
+    u"الكيماوي",
+    u"الاختناق",
+    u"الغازات السامة",
+    u"السام"
+]
+
+def call(sentence, keywords=keywords):
+    matches = filter(lambda keyword: keyword in sentence, keywords)
+
+    if any(matches):
+        print "Match on " + str(matches)
+        return matches
+    else:
+        print "Do nothing. No keyword match."
+        return []
